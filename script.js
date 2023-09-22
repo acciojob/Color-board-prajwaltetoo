58c6be084b468650a73dd45bb3574568d59156d2
prajwaltetoo <119670042+prajwaltetoo@users.noreply.github.com>
2023-09-22 18:16:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
+    const container = document.getElementById("color-board");
+
+    // Generate 800 boxes dynamically
+    for (let i = 0; i < 800; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+
+        // Add a mouseover event listener to revert the color after 1 second
+        square.addEventListener("mouseover", function () {
+            setTimeout(function () {
+                square.style.backgroundColor = "#ccc"; // Revert to the original color
+            }, 1000); // 1000 milliseconds = 1 second
+        });
+
+        container.appendChild(square);
+    }
+});
+
